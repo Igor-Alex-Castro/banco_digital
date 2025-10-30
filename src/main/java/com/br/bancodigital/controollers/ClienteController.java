a08db932f9a373dfd6ee5572345c5c8dc57e9966
Igor Alex de Castro <igoralex8701@gmail.com>
2025-10-30 15:39:05 -0300

    Criado ApiResponse, modelo de retorno do ResponseEntity

--- src/main/java/com/br/bancodigital/controollers/ClienteController.java
+++ src/main/java/com/br/bancodigital/controollers/ClienteController.java
@@ -1,5 +1,7 @@
 package com.br.bancodigital.controollers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -7,8 +9,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.br.bancodigital.dto.ApiResponse;
 import com.br.bancodigital.models.Cliente;
 import com.br.bancodigital.services.ClienteService;
+
 
 import jakarta.validation.Valid;
 
@@ -26,10 +30,16 @@ public class ClienteController {
 	}
 	
 	@PostMapping()
-	public ResponseEntity<Cliente> salvar(@RequestBody @Valid Cliente cliente){
+	public ResponseEntity<ApiResponse<Cliente>> salvar(@RequestBody @Valid Cliente cliente){
 		
-		clienteService.salvar(cliente);
+		Cliente clieteCriado = clienteService.salvar(cliente);
+		 
+		ApiResponse<Cliente> response = new ApiResponse<>(
+				 HttpStatus.CREATED.value(),
+				 	"Cliente criado com sucesso",
+				 	clieteCriado
+				 );
 		
-		return null;
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
 	}
 }
